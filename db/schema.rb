09bdd575b7efdef2d2955d0db2bb21b05ad330cd
Irene Fuentes <fuentes.acedo1.@gmail.com>
2016-06-06 10:14:57 +0100

    created join table for users and countries

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160603144512) do
+ActiveRecord::Schema.define(version: 20160606091414) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,11 @@ ActiveRecord::Schema.define(version: 20160603144512) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "countries_users", id: false, force: :cascade do |t|
+    t.integer "user_id",    null: false
+    t.integer "country_id", null: false
   end
 
   create_table "users", force: :cascade do |t|
